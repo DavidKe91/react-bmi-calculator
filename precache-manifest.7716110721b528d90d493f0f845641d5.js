64b309c64f079e6799e40370beba9794144bd7ed
DavidKe91 <davidgkennedy@hotmail.com>
2020-03-09 22:52:15 +0000

    Updates

---
+++ precache-manifest.7716110721b528d90d493f0f845641d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8915189245aaedd2b445e6d93df8294f",
+    "url": "/react-bmi-calculator/index.html"
+  },
+  {
+    "revision": "53a4adcf16563c32e85a",
+    "url": "/react-bmi-calculator/static/css/main.e9590225.chunk.css"
+  },
+  {
+    "revision": "a40688946880c78fbd51",
+    "url": "/react-bmi-calculator/static/js/2.17a6d555.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-bmi-calculator/static/js/2.17a6d555.chunk.js.LICENSE"
+  },
+  {
+    "revision": "53a4adcf16563c32e85a",
+    "url": "/react-bmi-calculator/static/js/main.48928477.chunk.js"
+  },
+  {
+    "revision": "c1d591e819b9cad58b36",
+    "url": "/react-bmi-calculator/static/js/runtime-main.4628aaf6.js"
+  }
+]);
